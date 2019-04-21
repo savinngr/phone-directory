195ebcc242f463d6de4e006a8c869d3b8f846efb
savinngr <savinngr@gmail.com>
2019-04-21 18:32:32 +0530

    Created Sample enteries for Phonebook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <button> Add </button>
-        <div>
-          <span> Name </span><br />
-          <span> Phone </span>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+          <div className="grid-container">
+            <span className="grid-item">Savin</span>
+            <span className="grid-item">888888</span>
+          </div>
+          <div className="grid-container">
+            <span className="grid-item">Suhani</span>
+            <span className="grid-item">999999</span>
+          </div>
+
         </div>
       </div>
     );
